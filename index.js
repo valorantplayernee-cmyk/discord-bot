958eb5b669a24c87798fa6e34ba05f49c82174c3
valorantplayernee-cmyk <valorantplayernee@gmail.com>
2026-03-22 14:13:46 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-intents: [
-GatewayIntentBits.Guilds,
-GatewayIntentBits.GuildMessages,
-GatewayIntentBits.MessageContent
-]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
-client.on("ready", () => {
-  console.log( Online: ${client.user.tag});
-  console.log(`✅ Online: ${client.user.tag}`);
+client.once('ready', () => {
+  console.log(`Online: ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('pong 🏓');
+  }
 });
 
 client.login(process.env.TOKEN);
