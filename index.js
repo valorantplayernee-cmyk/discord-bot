9e65ef8a78132979f2d4e76debf3d3d352bcba65
valorantplayernee-cmyk <valorantplayernee@gmail.com>
2026-03-22 13:09:23 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on("ready", () => {
+  console.log( Online: ${client.user.tag});
+});
+
+client.login(process.env.TOKEN);
