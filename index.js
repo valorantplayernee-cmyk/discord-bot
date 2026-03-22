d64d55991f39c924e6847de4371d79d4e774feed
valorantplayernee-cmyk <valorantplayernee@gmail.com>
2026-03-22 14:10:04 +1100

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+intents: [
+GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildMessages,
+GatewayIntentBits.MessageContent
+]
 });
 
 client.on("ready", () => {
+  console.log( Online: ${client.user.tag});
   console.log(`✅ Online: ${client.user.tag}`);
 });
 
